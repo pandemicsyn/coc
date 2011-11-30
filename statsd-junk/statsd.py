c0df6e9e6abf1566dac46fe47ea6d0cbb595c8b6
Florian Hines <florian.hines@gmail.com>
2011-11-30 08:23:00 +0000

    half way there.

--- statsd-junk/statsd.py
+++ statsd-junk/statsd.py
@@ -4,19 +4,23 @@ from eventlet.queue import Queue
 import time
 import re
 
+
 counters = {}
 stats_seen = 0
+debug = False
 
 def report_stats(payload):
+    if debug:
+        print "reporting stats"
     try:
         with eventlet.Timeout(5, True) as timeout:
             graphite = socket.socket()
             graphite.connect(("127.0.0.1", 5555))
+            stime = time.time()
             graphite.sendall(payload)
             graphite.close()
-            print "sent payload to graphite"
     except Exception as e:
-        print e
+        print "error connecting to graphite: %s" % e
 
 
 def stats_flush():
@@ -26,8 +30,9 @@ def stats_flush():
     payload = None
     while True:
         eventlet.sleep(flush_interval)
-        print "current counters: %s" % counters
-        print "flushing to graphite"
+        if debug:
+            print "current counters: %s" % counters
+            print "flushing to graphite"
         for item in counters:
             stats = 'stats.%s %s %s\n' % (item, counters[item] / flush_interval, tstamp)
             stats_counts = 'stats_counts.%s %s %s\n' % (item, counters[item], tstamp) 
@@ -38,16 +43,17 @@ def stats_flush():
 
 def main():
     global counters
+    global stats_seen
     keycheck = re.compile(r'\s+|/|[^a-zA-Z_\-0-9\.]')
     ratecheck = re.compile('^@([\d\.]+)')
-    eventlet.spawn(stats_flush)
-    global counter
+    eventlet.spawn_n(stats_flush)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     addr = ('127.0.0.1', 8125)
     sock.bind(addr)
     buf = 8192
     print "Listening on %s:%d" % addr
     while 1:
+        sample_rate = 1
         data, addr = sock.recvfrom(buf)
         if not data:
             break
@@ -61,20 +67,21 @@ def main():
                     if fields[1] is "ms":
                         print "error: no timer support yet"
                     else:
-                        sample_rate = 1
-                        try:
-                            sample_rate = ratecheck.match(fields[2]).groups()[0]
-                        except AttributeError:
-                            #y u pass @ symbol ?
-                            pass
+                        #checking if theres a sample rate
+                        if len(fields) is 3:
+                            try:
+                                sample_rate = ratecheck.match(fields[2]).groups()[0]
+                            except (IndexError, AttributeError):
+                                #y u no pass @ symbol ?
+                                print "error: bad sample rate field"
+                                break
+
                         if key not in counters:
                             counters[key] = 0
                         try:
-                            counters[key] = float(fields[0] or 1) * (1 / float(sample_rate))
-                        except ValueError:
-                            print "error: bad counter data"
-                        except ZeroDivisionError:
-                            print "error: bad sample rate"
+                            counters[key] += float(fields[0] or 1) * (1 / float(sample_rate))
+                        except (ValueError, ZeroDivisionError) as e:
+                            print e
                 else:
                     print "error: invalid"
             else:
