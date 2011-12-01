6b87ea7c2abd521ff97b56e28034ecd9e893f9bb
Florian Hines <florian.hines@gmail.com>
2011-12-01 08:01:31 +0000

    cleanup

--- statsd-junk/statsd.py
+++ statsd-junk/statsd.py
@@ -1,13 +1,12 @@
 import eventlet
 from eventlet.green import socket
-from eventlet.queue import Queue
 import time
 import re
 
-
 counters = {}
 stats_seen = 0
-debug = False
+debug = True
+
 
 def report_stats(payload):
     if debug:
@@ -16,29 +15,33 @@ def report_stats(payload):
         with eventlet.Timeout(5, True) as timeout:
             graphite = socket.socket()
             graphite.connect(("127.0.0.1", 5555))
-            stime = time.time()
             graphite.sendall(payload)
             graphite.close()
-    except Exception as e:
-        print "error connecting to graphite: %s" % e
+    except Exception as err:
+        print "error connecting to graphite: %s" % err
 
 
 def stats_flush():
     tstamp = int(time.time())
     flush_interval = 10 #seconds not milli
     global counters
-    payload = None
+    payload = []
     while True:
         eventlet.sleep(flush_interval)
         if debug:
+            print "seen %d stats so far." % stats_seen
             print "current counters: %s" % counters
             print "flushing to graphite"
         for item in counters:
-            stats = 'stats.%s %s %s\n' % (item, counters[item] / flush_interval, tstamp)
-            stats_counts = 'stats_counts.%s %s %s\n' % (item, counters[item], tstamp) 
-            payload = "".join([stats, stats_counts])
-            counters[item] = 0 
-        report_stats(payload)
+            stats = 'stats.%s %s %s\n' % (item,
+                        counters[item] / flush_interval, tstamp)
+            stats_counts = 'stats_counts.%s %s %s\n' % (item,
+                                counters[item], tstamp)
+            payload.append(stats)
+            payload.append(stats_counts)
+            counters[item] = 0
+        if payload:
+            report_stats("".join(payload))
 
 
 def main():
@@ -53,7 +56,7 @@ def main():
     buf = 8192
     print "Listening on %s:%d" % addr
     while 1:
-        sample_rate = 1
+        sample_rate = 1.0
         data, addr = sock.recvfrom(buf)
         if not data:
             break
@@ -63,34 +66,34 @@ def main():
                 key = keycheck.sub('_', bits[0])
                 print "got key: %s" % key
                 fields = bits[1].split("|")
-                if fields[1]:
+                field_count = len(fields)
+                if field_count >= 2:
                     if fields[1] is "ms":
-                        print "error: no timer support yet"
-                    else:
-                        #checking if theres a sample rate
-                        if len(fields) is 3:
-                            try:
-                                sample_rate = ratecheck.match(fields[2]).groups()[0]
-                            except (IndexError, AttributeError):
-                                #y u no pass @ symbol ?
-                                print "error: bad sample rate field"
-                                break
-
-                        if key not in counters:
-                            counters[key] = 0
+                        print "error: no timer support yet."
+                    elif fields[1] is "c":
                         try:
-                            counters[key] += float(fields[0] or 1) * (1 / float(sample_rate))
-                        except (ValueError, ZeroDivisionError) as e:
-                            print e
+                            if key not in counters:
+                                counters[key] = 0
+                            if field_count is 3:
+                                if ratecheck.match(fields[2]):
+                                    sample_rate = float(fields[2].lstrip("@"))
+                                else:
+                                    raise Exception("bad sample rate.")
+                            counters[key] += float(fields[0] or 1) * \
+                                (1 / float(sample_rate))
+                        except Exception as err:
+                            print "error decoding packet: %s" % err
+                        stats_seen += 1
+                    else:
+                        print "error: unsupported stats type"
                 else:
-                    print "error: invalid"
+                    print "error: not enough fields received"
             else:
-                print "error: invalid"
-            stats_seen += 1 
+                print "error: invalid request"
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print "\nReceived %d events" % stats_seen
-        print '\n'
+        print "\nReceived %d events\n" % stats_seen
